d0f2f3ef09acf21fba8bf3abe54fdd9a01af6982
PhanThiHoaiAnh <anhphan0327@gmail.com>
2024-05-08 18:17:22 +0700

    Done setup reactjs

---
+++ frontend/doantn_0106_dichvutieccuoi/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<h1>Dịch vụ tiệc cưới VP</h1>);
